897c84db4ad2867d05ceb021bc8e04a4d907ce55
Kyle Nweeia <kyle.nweeia@gmail.com>
2025-07-14 21:42:45 -0600

    Add Login page

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -2,9 +2,11 @@
 use dioxus::prelude::*;
 
 fn main() {
-    dioxus::launch(|| rsx! {
-        body {
-            Router::<Route> {}
+    dioxus::launch(|| {
+        rsx! {
+            body {
+                Router::<Route> {}
+            }
         }
     });
 }
@@ -13,10 +15,32 @@ fn main() {
 enum Route {
     #[route("/")]
     Home,
+    #[route("/login")]
+    Login,
 }
 
 fn Home() -> Element {
     rsx! {
         h1 { "Homeschool HQ" }
+        Link { to: Route::Login {}, class: "nav-btn", "Login" }
+    }
+}
+
+fn Login() -> Element {
+    let onsubmit = move |evt: FormEvent| async move {};
+
+    rsx! {
+        h1 { "Login" }
+        form { onsubmit,
+            label { "Username" }
+            br {}
+            input { r#type: "text", id: "username", name: "username" }
+            br {}
+            label { "Password" }
+            br {}
+            input { r#type: "password", id: "password", name: "password" }
+            br {}
+            button { "Sign in" }
+        }
     }
 }
